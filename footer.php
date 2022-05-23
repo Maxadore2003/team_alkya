1908bd5eabaca79b884797c2f9300e97dbe27483
Maxadore <maxime.desnoues@orange.fr>
2022-05-23 16:37:36 +0200

    footer

--- footer.php
+++ footer.php
@@ -1,19 +1,42 @@
 <footer class="footer">
 
-    <div class="social-networks"></div>
+    <div class="social-networks">
 
         <a href="#">
-            <img src="img/instagramme.png" alt=" Logo Instagramme">
+            <img src="img/instagram.png" alt="Logo Instagram" width="50">
         </a>
         <a href="#">
-            <img src="img/twitter.png" alt="Logo Twitter">
+            <img src="img/twitter.png" alt="Logo Twitter" width="50">
         </a>
         <a href="#">
-            <img src="img/twitch.png" alt="Logo Twitch">
+            <img src="img/twitch.png" alt="Logo Twitch" width="50">
         </a>
         <a href="#">
-            <img src="img/facebook.png" alt="Logo Facebook">
+            <img src="img/facebook.png" alt="Logo Facebook" width="50">
         </a>
+
+    </div>
+
+    <div class="about">
+
+        <p class="text-about">About</p>
+        <a>Actus</a>
+        <a>Teams</a>
+        <a>Qui sommes-nous ?</a>
+
+    </div>
+
+    <div class="informations">
+
+        <a>CGU</a>
+        <a>Mentions légales</a>
+        <a>Politique de confidentialité</a>
+
+    </div>
+
+    <div class="logo-black-alkya">
+
+        <img src="img/logo-black-alkya.png" alt="Logo Black Alkya" width="100">
 
     </div>
 
