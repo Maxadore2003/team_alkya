0b481bff0a4c16c7a4f0d92ac456bf23cc27a1ce
Maxadore <maxime.desnoues@orange.fr>
2022-05-23 15:16:18 +0200

    header/footer

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+
+    <div class="social-networks"></div>
+
+        <a href="#">
+            <img src="img/instagramme.png" alt=" Logo Instagramme">
+        </a>
+        <a href="#">
+            <img src="img/twitter.png" alt="Logo Twitter">
+        </a>
+        <a href="#">
+            <img src="img/twitch.png" alt="Logo Twitch">
+        </a>
+        <a href="#">
+            <img src="img/facebook.png" alt="Logo Facebook">
+        </a>
+
+    </div>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,24 @@
+<header class="header">
+
+    <a class="logo">
+        <img src="img/logo.png" alt="Logo">
+    </a>
+
+    <nav class="nav">
+        <ul>
+            <li>
+                <a href="#">ACTUS</a>
+            </li>
+            <li>
+                <a href="#">ABOUT US</a>
+            </li>
+            <li>
+                <a href="#">ÉQUIPES</a>
+            </li>
+            <li>
+                <a href="#">SHOP</a>
+            </li>
+        </ul>
+    </nav>
+
+</header>
