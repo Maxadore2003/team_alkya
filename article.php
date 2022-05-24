15b04523f410af4a7810c24b320dce1cc409cbf0
Maxadore <maxime.desnoues@orange.fr>
2022-05-24 17:35:53 +0200

    page actus / page article

---
+++ article.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/article.css">
+        <title>Article</title>
+    </head>
+
+    <body>
+
+        <div class="illustration">
+            <img src="img/illustration-article.png" alt="Illustration" width="1000">
+        </div>
+
+        <div class="content">
+            <h2>La rentabilité de l'esport</h2>
+            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. </p>
+        </div>
+        
+    </body>
+
+</html>
---
+++ news.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/header.css">
+        <link rel="stylesheet" href="./css/news.css">
+        <link rel="stylesheet" href="./css/footer.css">
+        <title>Actualités</title>
+    </head>
+
+    <body>
+
+        <?php
+            
+            // include "./includes/header.php";
+            // include "./includes/footer.php";
+        
+        ?>
+
+        <h1>Actualités</h1>
+
+        <div class="illustrations">
+            <div class="first-column">
+                <img src="img/illustration-news.png" alt="Illustration 1" width="300">
+                <p>La rentabilité dans l'esport</p>
+                <img src="img/illustration-news.png" alt="Illustration 2" width="300">
+                <p>La rentabilité dans l'esport</p>
+                <img src="img/illustration-news.png" alt="Illustration 3" width="300">
+                <p>La rentabilité dans l'esport</p>
+            </div>
+            <div class="second-column">
+                <img src="img/illustration-news.png" alt="Illustration 4" width="300">
+                <p>La rentabilité dans l'esport</p>
+                <img src="img/illustration-news.png" alt="Illustration 5" width="300">
+                <p>La rentabilité dans l'esport</p>
+                <img src="img/illustration-news.png" alt="Illustration 6" width="300">
+                <p>La rentabilité dans l'esport</p>
+            </div>
+        </div>
+
+        <div class="squares">
+            <div class="square">
+                <p>1</p>
+            </div>
+            <div class="square">
+                <p>2</p>
+            </div>
+            <div class="square">
+                <p>3</p>
+            </div>
+            <div class="square">
+                <p>4</p>
+            </div>
+            <div class="square">
+                <p>5</p>
+            </div>
+        </div>
+        
+    </body>
+
+</html>
