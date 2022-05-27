32209bd028bd19c6f19bd6e360d22242be105902
Maxadore <maxime.desnoues@orange.fr>
2022-05-27 09:20:31 +0200

    pages équipes / fiches équipes

---
+++ teams.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/teams.css">
+        <link rel="stylesheet" href="css/footer.css">
+        <title>Équipes</title>
+    </head>
+
+    <body>
+
+        <?php include "includes/header.php"; ?>
+    
+        <div class="title">
+            <h3>Nos Equipes</h3>
+        </div>
+
+        <div class="teams d-flex flex-wrap text-center">
+            <a class="logo-lol" href="">
+                <img src="img/logo-lol.png" alt="Logo LOL">
+            </a>
+            <a class="logo-valorant" href="">
+                <img src="img/logo-valorant.png" alt="Logo Valorant">
+            </a>
+            <a class="logo-rainbow-siege" href="">
+                <img src="img/logo-rainbow-siege.png" alt="Logo Rainbow Siege">
+            </a>
+            <a class="logo-rocket-league" href="">
+                <img src="img/logo-rocket-league.png" alt="Logo Rocket League">
+            </a>
+        </div>
+
+        <?php include "includes/footer.php"; ?>
+        
+    </body>
+
+</html>
---
+++ teams_sheet.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/header.css">
+        <link rel="stylesheet" href="css/teams_sheet.css">
+        <link rel="stylesheet" href="css/footer.css">
+        <title>Fiche Équipes</title>
+    </head>
+
+    <body>
+
+        <?php include "includes/header.php"; ?>
+
+        <div class="illustration">
+            <img src="img/team-lol.png" alt="Équipe LOL">
+            <p>Notre Line-Up</p>
+        </div>
+
+        <div class="players-title">
+            <p>Joueurs</p>
+        </div>
+
+        <!-- <div class="players">
+            <a class="images-player" href="">
+                <img src="img/flag.png" alt="Flag">
+                <img src="img/text-martin-larsson.png" alt="Texte Martin Larsson">
+            </a>
+        </div> -->
+
+        <div class="coach-title">
+            <p>Coach</p>
+        </div>
+
+        <?php include "includes/footer.php"; ?>
+        
+    </body>
+
+</html>
