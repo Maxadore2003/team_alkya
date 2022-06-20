f51fe5d72e6e1289a779a1aa89d3e8de8b68e5a0
Maxadore <maxime.desnoues@orange.fr>
2022-06-20 12:56:15 +0200

    css page connection

--- admin/connection.php
+++ admin/connection.php
@@ -6,20 +6,22 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/header_admin.css">
         <link rel="stylesheet" href="../css/connection.css">
         <title>Connexion</title>
     </head>
 
     <body>
 
-        <h1 class="text-center">Connexion</h1>
-
-        <form class="text-center" action="login.php" method="post">
-            <input type="text" name="pseudo" id="pseudo" placeholder="Votre Pseudo" required> <br>
-            <input type="password" name="password" id="password" placeholder="Votre Mot de Passe" required> <br>
-            <input type="submit" name="send" id="send" value="Connexion">
-            <?php if (isset($ConnexionError)) echo "<div style='color: red; font-weight: bold'>$ConnexionError</div>" ?>
+        <form action="login.php" method="post">
+            <h1>Connexion</h1>
+            <div class="inputs">
+                <input type="text" name="pseudo" id="pseudo" placeholder="Votre Pseudo" required>
+                <input type="password" name="password" id="password" placeholder="Votre Mot de Passe" required>
+            </div>
+            <div class="button" align="center">
+                <button type="submit" name="send" id="send">Connexion</button>
+                <?php if (isset($ConnexionError)) echo "<div style='color: red; font-weight: bold'>$ConnexionError</div>" ?>
+            </div>
         </form>
         
     </body>
