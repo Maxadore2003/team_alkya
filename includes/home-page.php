586b1c7462d8c81a57aaa26d8ae842570d8a58fe
Maxadore <maxime.desnoues@orange.fr>
2022-07-21 18:00:04 +0200

    api twitch pour live de team alkya

--- includes/home-page.php
+++ includes/home-page.php
@@ -180,6 +180,16 @@
             </a>
         </div>
     </div>
+    <div id="twitch-embed">
+        <script src="https://embed.twitch.tv/embed/v1.js"></script>
+        <script type="text/javascript">
+            new Twitch.Embed("twitch-embed", {
+                width: 1500,
+                height: 500,
+                channel: "teamalkya"
+            });
+        </script>
+    </div>
 </div>
 
 <script src="js/carrousel-homepage.js"></script>
